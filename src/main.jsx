ad7d5b98325bb304074cea68cec825f7ae66d432
Miguel <mmirandasanchez16@gmail.com>
2024-06-02 12:37:03 -0400

    copia de seguridad

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 import './styles.css'
@@ -7,3 +7,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App value={10} />
     </React.StrictMode>
 )
+
+//bases para react
