6d2c48fd5347037aed826e54adaa8553a46a8a24
Miguel <mmirandasanchez16@gmail.com>
2024-06-04 09:25:46 -0400

    copia de seguridad

---
+++ src/base de pruebas/06-deses-obj.js
@@ -0,0 +1,18 @@
+
+const clave = 1234;
+const edad = 45;
+export const usContext = ({ clave, edad}) => {
+
+    
+    return {
+        nombreClave: clave,
+        anios: edad,
+        latlng: {
+            lat: 14.1232,
+            lng: -12.3232
+        }
+    }
+
+}
+
+
